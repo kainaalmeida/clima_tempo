6206c23d2b02aabdbda8f32a73680095dc740a4b
Kainã Fontoura Almeida <kainafa@hotmail.com>
2020-12-06 21:23:22 -0300

    first commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:clima_tempo/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clima Tempo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:clima_tempo/app_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
+
